8a3152faf99c973b88847f7389c85803d7d508b9
Northpointmedia <mbasevim@gmail.com>
2026-07-30 13:23:15 -0400

    Debug Stripe key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,6 +4,11 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("Missing STRIPE_SECRET_KEY environment variable.");
 }
 
+console.log(
+  "Stripe key prefix:",
+  process.env.STRIPE_SECRET_KEY.substring(0, 20)
+);
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   appInfo: {
     name: "Basevi Solutions",
